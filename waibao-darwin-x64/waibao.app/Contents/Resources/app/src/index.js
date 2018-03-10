a624ddcc4a81c510054b5b1a8dee8b33129809ef
neatLines <neatlinefu@gmail.com>
2018-03-10 10:14:23 +0800

    add homepage

--- waibao-darwin-x64/waibao.app/Contents/Resources/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
-import Router from './pages/router';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
